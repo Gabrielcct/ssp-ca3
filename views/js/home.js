8ff77893303df1740bd0f2f48cb8431640d2606e
Gabriel <2021240@student.cct.ie>
2022-12-25 18:42:16 +0000

    Home page added styles and colors

--- views/js/home.js
+++ views/js/home.js
@@ -4,58 +4,105 @@
 * here goes home page content - variable need to be called as route path!
 */
 const home =`
-        <div class="top row">
+        <!-- TOP DIV -->
+        <div class="top row margin-top-120 margin-bottom-120 position-relative">
+                <!-- Background icon -->
+                <i class="fa-solid fa-paw position-absolute opacity-25 app-color paw-icon"></i>
+                <!-- TOP LEFT DIV -->
                 <div class="top-left col-6">
-                        <h1> Your pet is part of your family!</h1>
-                        <p>Give your pet best product it deserves. Find on our web shop</p>
+                        <!-- Main text -->
+                        <h1 class="fw-normal margin-bottom-35"> Your cat is part of your family!</h1>
+                        <!-- text -->
+                        <p class="margin-top-25 margin-bottom-35">Give your cat best product it deserves. Find on our web shop</p>
+                        <!-- button -->
                         <button class="btn btn-primary">Shop</button>
                 </div>
+                <!-- TOP RIGHT  DIV -->
                 <div class="top-right col-6">
-                        image goes here
+                        <!-- image -->
+                        <img src="./assets/kitten.jpg" width="auto" height="100%">
                 </div>
         </div>
-        <div class="bottom row">
-                <div class="bottom-top col-12">
-                        <h1> What can we do for you</h1>  
-                        <p>We took care of your family. See list of our services</p>
+
+        <!-- BOTTOM DIV -->
+        <div class="bottom row margin-top-120 margin-bottom-120">
+                <!-- BOTTOM TOP DIV -->
+                <div class="bottom-top col-12 text-center">
+                        <!-- Main text -->
+                        <h1 class="fw-normal"> What We Can Do</h1>  
+                        <!-- text -->
+                        <p class="margin-top-25 margin-bottom-60">We took care of your family. See list of our services</p>
                 </div>
+                <!-- BOTTOM BOTTOM DIV - hold cards -->
                 <div class="bottom-bottom row">
-                        <div class="card col-md-3 col-sm-12" >
-                                <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                                        p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" class="card-link">Card link</a>
-                                        <a href="#" class="card-link">Another link</a>
+                        <!-- CARD HOLDER -->
+                        <div class="col-md-3 col-sm-12" >
+                                <!-- CARD -->
+                                <div class="card">
+                                        <!-- bootstrap 5 card body -->
+                                        <div class="card-body text-center">
+                                                <!-- circle wiyh icon -->
+                                                <div class="img-crcle img-circle-blue position-relative">
+                                                        <i class="fa-solid fa-bowl-food position-absolute fs-1 img-circle-icon"></i>
+                                                </div>    
+                                                <!-- card title -->    
+                                                <h5 class="card-title">Royal Canin food</h5>
+                                                <!-- card text -->   
+                                                <p class="card-text">Royal Canin's mission has always been to promote nutrition and health. This is characterised by an exact balance of fifty different nutrients (proteins, fats, carbohydrates, amino acids, fatty acids, mineral salts, vitamins and essential elements). The formulation of their food is made up of a complex blend that guarantees a consistent nutrition... </p>
+                                        </div>
                                 </div>
-                        </div>
-                        <div class="card col-md-3 col-sm-12">
-                                <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                                        p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" class="card-link">Card link</a>
-                                        <a href="#" class="card-link">Another link</a>
+                        </div>        
+                        <!-- CARD HOLDER -->
+                        <div class="col-md-3 col-sm-12" >
+                                <!-- CARD -->
+                                <div class="card">
+                                        <!-- bootstrap 5 card body -->
+                                        <div class="card-body text-center">
+                                                <!-- circle wiyh icon -->
+                                                <div class="img-crcle img-circle-orange position-relative">
+                                                        <i class="fa-solid fa-volleyball position-absolute fs-1 img-circle-icon"></i>
+                                                </div>    
+                                                <!-- card title -->    
+                                                <h5 class="card-title">Cat Toys and towers</h5>
+                                                <!-- card text -->   
+                                                <p class="card-text">Royal Canin's mission has always been to promote nutrition and health. This is characterised by an exact balance of fifty different nutrients (proteins, fats, carbohydrates, amino acids, fatty acids, mineral salts, vitamins and essential elements). The formulation of their food is made up of a complex blend that guarantees a consistent nutrition... </p>
+                                        </div>
                                 </div>
-                        </div>
-                        <div class="card col-md-3 col-sm-12">
-                                <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                                        p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" class="card-link">Card link</a>
-                                        <a href="#" class="card-link">Another link</a>
+                        </div>       
+                        <!-- CARD HOLDER -->
+                        <div class="col-md-3 col-sm-12" >
+                                <!-- CARD -->
+                                <div class="card">
+                                        <!-- bootstrap 5 card body -->
+                                        <div class="card-body text-center">
+                                                <!-- circle wiyh icon -->
+                                                <div class="img-crcle img-circle-green position-relative">
+                                                        <i class="fa-solid fa-chart-column position-absolute fs-1 img-circle-icon"></i>
+                                                </div>    
+                                                <!-- card title -->    
+                                                <h5 class="card-title">Cat litter</h5>
+                                                <!-- card text -->   
+                                                <p class="card-text">Royal Canin's mission has always been to promote nutrition and health. This is characterised by an exact balance of fifty different nutrients (proteins, fats, carbohydrates, amino acids, fatty acids, mineral salts, vitamins and essential elements). The formulation of their food is made up of a complex blend that guarantees a consistent nutrition... </p>
+                                        </div>
                                 </div>
-                        </div>
-                        <div class="card col-md-3 col-sm-12" >
-                                <div class="card-body">
-                                        <h5 class="card-title">Card title</h5>
-                                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                                        p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" class="card-link">Card link</a>
-                                        <a href="#" class="card-link">Another link</a>
+                        </div>       
+                        <!-- CARD HOLDER -->
+                        <div class="col-md-3 col-sm-12" >
+                                <!-- CARD -->
+                                <div class="card">
+                                        <!-- bootstrap 5 card body -->
+                                        <div class="card-body text-center">
+                                                <!-- circle wiyh icon -->
+                                                <div class="img-crcle img-circle-red position-relative">
+                                                        <i class="fa-solid fa-pump-medical position-absolute fs-1 img-circle-icon"></i>
+                                                </div>    
+                                                <!-- card title -->    
+                                                <h5 class="card-title">Hygene products</h5>
+                                                <!-- card text -->   
+                                                <p class="card-text">Royal Canin's mission has always been to promote nutrition and health. This is characterised by an exact balance of fifty different nutrients (proteins, fats, carbohydrates, amino acids, fatty acids, mineral salts, vitamins and essential elements). The formulation of their food is made up of a complex blend that guarantees a consistent nutrition... </p>
+                                        </div>
                                 </div>
-                        </div>
+                        </div>       
                 </div>
         </div>
 `;
